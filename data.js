2a3993e0c5ea9da391c249708dd2a8740d8db5db
GiannaDavila <giannagiannadavila@gmail.com>
2022-10-17 16:13:58 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,10 @@
+export default [{
+    "name": "Gianna",
+    "age" : 18,
+    "'birthday": "September 1, 2004"
+},{
+    "name": "Todd Albert",
+    "age": 47,
+    "'birthday": "October 17, 1975"
+
+}]
